0ae823594bfb6ae7d2f3e1b0a406ddd855b0d671
shipalibhandary <shipalibhandary@gmail.com>
2025-02-25 20:54:28 +0530

    Update Perfect_Number.py

--- Python/Perfect_Number.py
+++ Python/Perfect_Number.py
@@ -1,1 +1,10 @@
+n=int(input("Enter a number:"))
+sum=0
+for i in range(1,n):
+    if(n%1==0):
+        sum+=i
+if(sum==n):
+    print(n,"is a perfect number")
+else:
+    print(n,"is not a perfect number")
 
