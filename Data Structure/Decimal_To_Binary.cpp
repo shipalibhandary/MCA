fd25eda72d30a3841189e9a33c4e6028818809fd
shipalibhandary <shipalibhandary@gmail.com>
2025-03-06 19:52:22 +0530

    Update Decimal_To_Binary.cpp

--- Data Structure/Decimal_To_Binary.cpp
+++ Data Structure/Decimal_To_Binary.cpp
@@ -1,1 +1,46 @@
+//Write a c++ program to convert decimal number to binary number using Stack data structure.
+#include<iostream>
+#define MAX 20
+using namespace std;
+
+class DecToBinary
+{
+    public:
+        int top;
+        int stk[MAX];
+        DecToBinary()
+        {
+            top=-1;
+        }
+        void push(int obj)
+        {
+            stk[++top]=obj;
+        }
+        int pop()
+        {
+            int obj;
+            return obj=stk[top--];
+        }
+};
+
+int main()
+{
+    DecToBinary db;
+    int num,n,rem,binary=0;
+    cout<<"Enter the number to be converted:";
+    cin>>num;
+    n=num;
+    while(n!=0)
+    {
+        rem=n%2;
+        db.push(rem);
+        n=n/2;
+    }
+    while(db.top!=-1)
+    {
+        binary=binary*10+db.pop();
+    }
+    cout<<"Binary value of "<<num<<" is:"<<binary;
+    return 0;
+}
 
